9bdd9b32267d4035fcca1e6f270f71c9c22964a2
diagroza <127187494+diagroza@users.noreply.github.com>
2023-05-22 17:32:55 +0300

    Homework_3_changes

---
+++ Homework_3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace ConsoleApplication4
+{
+    class Price
+    {
+        public void priceTotal(ref int price, ref int quantity)
+        {
+            //logical operations
+            int total = price * quantity;
+            Console.WriteLine("Total price is" + " " + total);
+            Console.ReadLine();
+        }
+    }
+    class Product
+    {
+        public static void Main(string[] args)
+        {
+            /* local variable definition */
+
+            int price = 9;
+            int quantity = 85;
+            //defining the object for the class marks
+
+            Price p = new Price();
+
+            //calling marks total method for calculation
+
+            p.priceTotal(ref price, ref quantity);
+        }
+    }
+}
